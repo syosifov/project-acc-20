10f3489b31f7e081bf2dff0279b4555a70afbc3e
Strashimir Yosifov <syosifov@gmail.com>
2020-04-28 21:21:47 +0300

    Initial Project State

---
+++ acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/Acc20Application.java
@@ -0,0 +1,13 @@
+package net.yosifov.filipov.training.accounting.acc20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Acc20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Acc20Application.class, args);
+	}
+
+}
---
+++ acc-20/src/test/java/net/yosifov/filipov/training/accounting/acc20/Acc20ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yosifov.filipov.training.accounting.acc20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Acc20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
