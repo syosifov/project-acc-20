09392968507a08618ba2e75c11041be85374bb7e
Strashimir Yosifov <syosifov@gmail.com>
2020-05-10 12:40:05 +0300

    Added swagger

--- acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/SwaggerConfig.java
+++ acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/SwaggerConfig.java
@@ -2,17 +2,17 @@ package net.yosifov.filipov.training.accounting.acc20;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@Configuration
-//@EnableSwagger2
+@Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
-    //@Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2);
-//
-//    }
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2);
+
+    }
 }
