add5dced3c6e33e42edeef4d25e85e107701cc46
Strashimir Yosifov <syosifov@gmail.com>
2020-05-14 11:23:05 +0300

    @EnableTransactionManagement

--- acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/controllers/CompaniesContr.java
+++ acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/controllers/CompaniesContr.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 
 @RestController
 @EnableTransactionManagement
+// https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#transactions
 public class CompaniesContr {
 
     @Autowired
