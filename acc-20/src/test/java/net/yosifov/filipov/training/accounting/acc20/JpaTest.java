a21561d367f7e7ab5eaa0bc716ff14b4af5d426c
Strashimir Yosifov <syosifov@gmail.com>
2020-05-16 09:43:51 +0300

    Employee Test

--- acc-20/src/test/java/net/yosifov/filipov/training/accounting/acc20/JpaTest.java
+++ acc-20/src/test/java/net/yosifov/filipov/training/accounting/acc20/JpaTest.java
@@ -1,30 +1,23 @@
 package net.yosifov.filipov.training.accounting.acc20;
 
-import net.yosifov.filipov.training.accounting.acc20.entities.Company;
-import net.yosifov.filipov.training.accounting.acc20.repositories.CompaniesRep;
+import net.yosifov.filipov.training.accounting.acc20.entities.Employee;
+import net.yosifov.filipov.training.accounting.acc20.repositories.EmployeeRep;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @DataJpaTest
 public class JpaTest {
-
     @Autowired
-    private CompaniesRep companiesRep;
+    private EmployeeRep employeeRep;
 
     @Test
-    @Transactional
-    public void companyTest() {
-
-        //companiesRep.deleteAll();
-        companiesRep.save(new Company("Company 1", "Address 1", "1111111112"));
-        companiesRep.save(new Company("Company 1", "Address 1", "1111111113"));
-
-
-        List<Company> lst = companiesRep.findAll();
+    public void t1() {
+        employeeRep.save(new Employee("Jim", "Sale", 1000));
+        List<Employee> lst = employeeRep.findAll();
         System.out.println(lst);
+        System.out.println(lst.size());
     }
 }
