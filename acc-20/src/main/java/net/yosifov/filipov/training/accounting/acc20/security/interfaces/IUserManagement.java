66585b79bbd14ded2ff0ea8eb3728c160e410555
Ivan <zerorukb@gmail.com>
2020-05-25 20:40:30 +0300

    documentation update

--- acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/security/interfaces/IUserManagement.java
+++ acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/security/interfaces/IUserManagement.java
@@ -1,8 +1,11 @@
 package net.yosifov.filipov.training.accounting.acc20.security.interfaces;
 
+import net.yosifov.filipov.training.accounting.acc20.system.serialization.JSonParam;
 import net.yosifov.filipov.training.accounting.acc20.system.serialization.JSonResult;
 
 public interface IUserManagement {
+    /// this is good for when you need universal jdetails
+    JSonResult CreateUser(String userName, String Password, JSonParam param);
     JSonResult CreateUser(String userName, String Password, String Name);
     JSonResult DeleteUser(String userName);
     JSonResult DisableUser(String userName);
