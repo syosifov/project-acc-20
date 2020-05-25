5f2131dfa27e62d9c521c18ba44fd5e4f9066be7
Ivan <zerorukb@gmail.com>
2020-05-25 20:41:46 +0300

    Create user with json paramter

--- acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/security/UserManagementService.java
+++ acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/security/UserManagementService.java
@@ -1,9 +1,15 @@
 package net.yosifov.filipov.training.accounting.acc20.security;
 
 import net.yosifov.filipov.training.accounting.acc20.security.interfaces.IUserManagement;
+import net.yosifov.filipov.training.accounting.acc20.system.serialization.JSonParam;
 import net.yosifov.filipov.training.accounting.acc20.system.serialization.JSonResult;
 
 public class UserManagementService implements IUserManagement {
+
+    @Override
+    public JSonResult CreateUser(String userName, String Password, JSonParam param) {
+        return null;
+    }
 
     @Override
     public JSonResult CreateUser(String userName, String Password, String Name) {
