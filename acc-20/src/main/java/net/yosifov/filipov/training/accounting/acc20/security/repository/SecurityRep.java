f261978bfe5fa42e723d80b7d29f3cf36bbca4f6
Ivan <zerorukb@gmail.com>
2020-05-30 22:37:07 +0300

    add security repositories

---
+++ acc-20/src/main/java/net/yosifov/filipov/training/accounting/acc20/security/repository/SecurityRep.java
@@ -0,0 +1,11 @@
+package net.yosifov.filipov.training.accounting.acc20.security.repository;
+
+
+//// Pomisli dali da nimam cateovory repositories
+//// naprimer security repostori moje da raboti samotistlno izobsho da ne e svyzrano s shetvoosddstvo
+/// security_domain every domain has
+/// - repor
+/// - secorury
+/// - controller
+public interface SecurityRep {
+}
